d4121b4bef83441414d2688d21b849fc03046a29
kmayuresh25 <mayureshkaluskar4@gmail.com>
2025-05-29 21:45:16 +0530

    Add files via upload

---
+++ Assignment17/program2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void Display(int iRow , int iCol)
+{
+   int i = 0;
+   int j = 0;
+
+   for(i=iRow;i>=1;i--)
+   {
+     for(j=1;j<=iCol;j++)
+     {
+        if(i==j)
+        {
+            printf("#\t");
+        }
+        else if(i>j)
+        {
+            printf("*\t");
+        }
+        else
+        {
+           printf("@\t");
+        }
+     }
+
+     printf("\n");
+
+   }
+}
+
+int main()
+{
+   int iVal1 = 0;
+   int iVal2 = 0;
+
+   printf("Enter the number of rows :");
+   scanf("%d",&iVal1);
+
+   printf("Enter the number of cols :");
+   scanf("%d",&iVal2);
+
+   Display(iVal1,iVal2);
+
+    return 0;
+}
